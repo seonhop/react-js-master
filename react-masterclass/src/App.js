393149433ea157c7da401ded9383e67f9f766ee1
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-10 23:59:30 -0400

    #2.4 Animations and Pseudo Selectors
    #Animations
    
    const animationName = keyframs``;
    const Variable = styled.tagName`animation: ${animation};`;
    
    #Defining CSS properties of a child tag inside a styled component
    
    const Variable = styled.tagName`childTag {CSS}`;
    
    #Pseudo Selectors
    const Variable = styled.tagName`childTag{&:hover {}}`;

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -4,16 +4,40 @@ const Wrapper = styled.div`
     display: flex;
 `;
 
+const animation = keyframes`
+    0% {
+        transform: rotate(0deg);
+        border-radius: 0px;
+    } 50% {
+        border-radius: 100px;
+    } 100% {
+        transform: rotate(360deg);
+        border-radius: 0px;
+    }
+`;
+
 const Box = styled.div`
     height: 200px;
     width: 200px;
     background-color: tomato;
+    animation: ${animation} 1s linear infinite;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+        font-size: 36px;
+        &:hover {
+            font-size: 50px;
+        }
+    }
 `;
 
 function App() {
     return (
         <Wrapper>
-            <Box />
+            <Box>
+                <span>happy</span>
+            </Box>
         </Wrapper>
     );
 }
