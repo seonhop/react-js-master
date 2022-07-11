a484360dceea0741f037f5bd8a280970c2c3e075
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-11 00:24:53 -0400

    #2.7 Themes
    in index.js file
    
    import {ThemeProvider} from "styled-components";
    
    const themeName {prop: value};
    
    <ThemeProvider theme = {themeName}>
    
    #Accessing theme components
    const Variable = styled.div`cssProp: ${(props) => props.theme.prop};`;

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,47 +1,20 @@
 import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
+    background-color: ${(props) => props.theme.backgroundColor};
     display: flex;
+    width: 100%;
+    height: 100vh;
 `;
 
-const animation = keyframes`
-    0% {
-        transform: rotate(0deg);
-        border-radius: 0px;
-    } 50% {
-        border-radius: 100px;
-    } 100% {
-        transform: rotate(360deg);
-        border-radius: 0px;
-    }
-`;
-
-const Emoji = styled.span`
-    font-size: 36px;
-`;
-
-const Box = styled.div`
-    height: 200px;
-    width: 200px;
-    background-color: tomato;
-    animation: ${animation} 1s linear infinite;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    ${Emoji} {
-        &:hover {
-            font-size: 50px;
-        }
-    }
+const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
     return (
         <Wrapper>
-            <Box>
-                <Emoji>🤤</Emoji>
-            </Box>
-            <Emoji>🤤</Emoji>
+            <Title>Hi</Title>
         </Wrapper>
     );
 }
--- react-masterclass/src/index.js
+++ react-masterclass/src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 
+const darkTheme = {
+    textColor: "whitesmoke",
+    backgroundColor: "#111",
+};
+
+const lightTheme = {
+    textColor: "#111",
+    backgroundColor: "whitesmoke",
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <ThemeProvider theme={darkTheme}>
+        <App />
+    </ThemeProvider>
+);
