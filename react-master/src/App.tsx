816946abeef7b769067b96b8befcd96696a9780d
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-12 16:16:04 -0400

    #3.2 Typing the Props
    defining the props' types
    
    interface VariableProps { prop: type;}

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -1,10 +1,17 @@
-import styled from "styled-components";
 import Circle from "./Circle";
+import styled from "styled-components";
+
+const Square = styled.div`
+    width: 200px;
+    height: 200px;
+    background-color: tomato;
+`;
 
 function App() {
     return (
         <div>
-            <Circle />
+            <Circle bgColor="teal" />
+            <Circle bgColor="tomato" />
         </div>
     );
 }
--- react-master/src/Circle.tsx
+++ react-master/src/Circle.tsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+interface PlayerShape {
+    name: string;
+    age: number;
+}
+
+const sayHello = (playerObj: PlayerShape) =>
+    `Hello ${playerObj.name} you are ${playerObj.age} years old`;
+
+sayHello({ name: "nico", age: 12 });
+
+interface ContainerProps {
+    bgColor: string;
+}
 
 interface CircleProps {
     bgColor: string;
 }
 
+const Container = styled.div<ContainerProps>`
+    width: 200px;
+    height: 200px;
+    background-color: ${(props) => props.bgColor};
+    border-radius: 100px;
+`;
+
 function Circle({ bgColor }: CircleProps) {
-    return <Container />;
+    return <Container bgColor={bgColor} />;
 }
 
 export default Circle;
