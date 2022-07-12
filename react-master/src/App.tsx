176732b5445edf0da722d7c2dcb48e9e5fe0e819
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-12 15:20:38 -0400

    setup
    didn't work

---
+++ react-master/src/App.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+    return (
+        <div>
+            <Circle />
+        </div>
+    );
+}
+
+export default App;
