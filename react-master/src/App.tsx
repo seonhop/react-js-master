65af9d68a16c68a7b58cb729e60fdc9643f13f3e
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-13 15:01:02 -0400

    .

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -1,20 +1,7 @@
-import styled from "styled-components";
-import React, { useState } from "react";
-
-const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-    color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
-    return (
-        <Container>
-            <H1>Hello</H1>
-        </Container>
-    );
+    return <Router />;
 }
 
 export default App;
